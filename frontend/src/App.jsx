681ae19fd0dfca58c3c99078693f9c55469eb4c6
Vu Huy Hoang <149637745+hoangvh17@users.noreply.github.com>
2024-09-24 22:54:19 +0700

    update app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Login } from "./pages/login/login.jsx";
+import { Lession } from "./pages/lession/lession.jsx";
 
 function App() {
     return (
@@ -7,7 +8,9 @@ function App() {
             <Router>
                 <Routes>
                     <Route path="/login" element={<Login />}></Route>
-                   
+                    <Route path="/lession" element={<Lession />}></Route>
+
+
                 </Routes>
             </Router>
         </>
