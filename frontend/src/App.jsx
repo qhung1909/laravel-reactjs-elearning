49d873a11084b5f8d11f4305ee917a766054125d
Vu Huy Hoang <149637745+hoangvh17@users.noreply.github.com>
2024-09-25 16:45:05 +0700

    update route( hone, header, footer, admin)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,27 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Login } from "./pages/login/login.jsx";
 import { Lession } from "./pages/lession/lession.jsx";
 import { Detail } from "./pages/detail/detail.jsx";
+import { Dashboard } from "./pages/admin/admin.jsx";
+import Home from "./pages/home/home.jsx";
+import Header from "./pages/header/header.jsx";
+import Footer from "./pages/footer/footer.jsx";
 
 function App() {
     return (
         <>
             <Router>
+                <Header />
                 <Routes>
+
+                    <Route path="/admin" element={<Dashboard />}></Route>
+
                     <Route path="/login" element={<Login />}></Route>
                     <Route path="/lession" element={<Lession />}></Route>
                     <Route path="/detail" element={<Detail />}></Route>
+                    <Route path="/home" element={<Home />}></Route>
 
                 </Routes>
+                <Footer />
             </Router>
         </>
     );
