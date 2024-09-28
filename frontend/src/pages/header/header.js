ac27154c0abe4a7eb976ba7cf652d1d4f5c18f38
Lâm Chấn Toàn <lamchantoan.sg@gmail.com>
2024-09-28 11:50:09 +0700

    change javascript reponsive header

---
+++ frontend/src/pages/header/header.js
@@ -0,0 +1,7 @@
+const navbarContent = document.getElementById('navbar-content')
+const toggleBtn = document.getElementById('toggle-btn')
+
+toggleBtn.addEventListener('click', () => {
+    navbarContent.classList.toggle('top-[70px]');
+})
+
