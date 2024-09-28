6c6a8e1b1d101b35a904fec628117517d81273c9
BaoHungg <86723931+BaoHungg@users.noreply.github.com>
2024-09-28 19:40:25 +0700

    update detail.js

--- frontend/src/pages/detail/detail.jsx
+++ frontend/src/pages/detail/detail.jsx
@@ -1,4 +1,5 @@
 import "./detail.css";
+import "../../components/js/detail.js";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
