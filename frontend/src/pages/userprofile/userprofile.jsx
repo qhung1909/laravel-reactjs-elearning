ad2999e0e9289abe34daf210705aa661c68d5a99
Lâm Chấn Toàn <lamchantoan.sg@gmail.com>
2024-10-15 21:53:52 +0700

    update useraccount

--- frontend/src/pages/userprofile/userprofile.jsx
+++ frontend/src/pages/userprofile/userprofile.jsx
@@ -1,12 +1,3 @@
-import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { Button } from "@/components/ui/button"
