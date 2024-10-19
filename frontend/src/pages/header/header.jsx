3cfe448875d4403c9a1905353d423f3d54005e3b
Lâm Chấn Toàn <lamchantoan.sg@gmail.com>
2024-10-19 13:33:16 +0700

    update header courses list

--- frontend/src/pages/header/header.jsx
+++ frontend/src/pages/header/header.jsx
@@ -37,7 +37,18 @@ export const Header = () => {
     const [user, setUser] = useState([]);
     const [loadingLogout, setLoadingLogout] = useState(false);
     const [loading, setLoading] = useState(false);
+    const categoryImages = {
+        javascript: "./src/assets/images/javascript.svg",
+        python: "./src/assets/images/python.svg",
+        reactjs: "./src/assets/images/reactjs.svg",
+        angular: "./src/assets/images/angular.svg",
+        css: "./src/assets/images/css.svg",
+        html: "./src/assets/images/html.svg",
+        next: "./src/assets/images/next.svg",
+        asp: "./src/assets/images/asp.svg",
+        nodejs: "./src/assets/images/nodejs.svg",
 
+    };
     const navigate = useNavigate();
 
     const logout = () => {
@@ -154,10 +165,13 @@ export const Header = () => {
             </div>
         ) : Array.isArray(categories) && categories.length > 0 ? (
             categories.map((item) => {
+                const categoryImage = categoryImages[item.slug] || "./src/assets/images/default.svg";
                 return (
-                    <div key={item.slug} className="px-5 hover:bg-gray-100 duration-300 cursor-pointer">
+                    <div key={item.slug} className="duration-300 cursor-pointer py-1">
                         <DropdownMenuItem>
-                            <Link className="flex gap-3 hover:text-yellow-400 duration-300 px-5 py-1 rounded-md">
+                            <Link className="flex gap-3 hover:text-yellow-400 duration-300 py-1 rounded-md">
+                                {/* tạo một mảng hình ảnh */}
+                                <img src={categoryImage} className="w-5" alt={item.name} />
                                 <span className="font-semibold text-base">{item.name}</span>
                             </Link>
                         </DropdownMenuItem>
@@ -219,10 +233,33 @@ export const Header = () => {
                                 <div className="navbar-content xl:static xl:min-h-fit  bg-white xl:flex xl:items-center px-10 max-xl:w-full gap-2 xl:block hidden">
                                     <ul className="items-center max-xl:pt-3 gap-3 flex text-base xl:text-base w-52">
                                         <li className="max-xl:mb-4">
-                                            <Link to="/courses" className="hover:text-gray-500">
-                                                Khóa học
-                                            </Link>
+                                            <DropdownMenu>
+                                                <DropdownMenuTrigger>
+                                                    <Link to="/courses" className="hover:text-gray-500 border-none">
+                                                        Khóa học
+                                                    </Link>
+                                                </DropdownMenuTrigger>
+                                                <DropdownMenuContent>
+                                                    <div className="grid grid-cols-2 gap-3 ">
+                                                        <div className="grid-1 border-r border-gray-100 px-5">
+                                                            <DropdownMenuLabel className="text-base text-blue-900 uppercase font-bold">Loại</DropdownMenuLabel>
+                                                            <div className=" duration-300 cursor-pointer">
+                                                                <DropdownMenuItem>
+                                                                    <Link to="/courses" className="flex gap-3 hover:text-yellow-400 duration-300 py-1 rounded-md">
+                                                                        <svg width="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 8C4 5.17157 4 3.75736 4.87868 2.87868C5.75736 2 7.17157 2 10 2H14C16.8284 2 18.2426 2 19.1213 2.87868C20 3.75736 20 5.17157 20 8V16C20 18.8284 20 20.2426 19.1213 21.1213C18.2426 22 16.8284 22 14 22H10C7.17157 22 5.75736 22 4.87868 21.1213C4 20.2426 4 18.8284 4 16V8Z" stroke="currentColor" strokeWidth="2"></path><path d="M19.8978 16H7.89778C6.96781 16 6.50282 16 6.12132 16.1022C5.08604 16.3796 4.2774 17.1883 4 18.2235" stroke="currentColor" strokeWidth="2"></path><path d="M8 7H16" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path><path d="M8 10.5H13" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path></svg>
+                                                                        <span className="font-semibold text-base">Tất cả khóa học</span>
+                                                                    </Link>
+                                                                </DropdownMenuItem>
 
+                                                            </div>
+                                                        </div>
+                                                        <div className="grid-2 px-5">
+                                                            <DropdownMenuLabel className=" text-base text-blue-900 uppercase font-bold">Danh mục</DropdownMenuLabel>
+                                                            {renderCategories()}
+                                                        </div>
+                                                    </div>
+                                                </DropdownMenuContent>
+                                            </DropdownMenu>
                                         </li>
                                         <li className="max-xl:mb-4">
                                             <Link to="/contact" className="hover:text-gray-500">
@@ -369,9 +406,33 @@ export const Header = () => {
                                                     {/* nav list */}
                                                     <ul className="max-xl:pt-3 gap-3 text-base xl:text-base text-left">
                                                         <li className="max-xl:mb-4">
-                                                            <Link to="/courses" className="hover:text-gray-500">
-                                                                Khóa học
-                                                            </Link>
+                                                            <DropdownMenu>
+                                                                <DropdownMenuTrigger>
+                                                                    <Link to="/courses" className="hover:text-gray-500 border-none">
+                                                                        Khóa học
+                                                                    </Link>
+                                                                </DropdownMenuTrigger>
+                                                                <DropdownMenuContent>
+                                                                    <div className="grid grid-cols-2 gap-3 ">
+                                                                        <div className="grid-1 border-r border-gray-100 px-5">
+                                                                            <DropdownMenuLabel className="text-base text-blue-900 uppercase font-bold">Loại</DropdownMenuLabel>
+                                                                            <div className=" duration-300 cursor-pointer">
+                                                                                <DropdownMenuItem>
+                                                                                    <Link to="/courses" className="flex gap-3 hover:text-yellow-400 duration-300 py-1 rounded-md">
+                                                                                        <svg width="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 8C4 5.17157 4 3.75736 4.87868 2.87868C5.75736 2 7.17157 2 10 2H14C16.8284 2 18.2426 2 19.1213 2.87868C20 3.75736 20 5.17157 20 8V16C20 18.8284 20 20.2426 19.1213 21.1213C18.2426 22 16.8284 22 14 22H10C7.17157 22 5.75736 22 4.87868 21.1213C4 20.2426 4 18.8284 4 16V8Z" stroke="currentColor" strokeWidth="2"></path><path d="M19.8978 16H7.89778C6.96781 16 6.50282 16 6.12132 16.1022C5.08604 16.3796 4.2774 17.1883 4 18.2235" stroke="currentColor" strokeWidth="2"></path><path d="M8 7H16" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path><path d="M8 10.5H13" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path></svg>
+                                                                                        <span className="font-semibold text-base">Tất cả khóa học</span>
+                                                                                    </Link>
+                                                                                </DropdownMenuItem>
+
+                                                                            </div>
+                                                                        </div>
+                                                                        <div className="grid-2 px-5">
+                                                                            <DropdownMenuLabel className=" text-base text-blue-900 uppercase font-bold">Danh mục</DropdownMenuLabel>
+                                                                            {renderCategories()}
+                                                                        </div>
+                                                                    </div>
+                                                                </DropdownMenuContent>
+                                                            </DropdownMenu>
                                                         </li>
                                                         <li className="max-xl:mb-4">
                                                             <Link to="/contact" className="hover:text-gray-500">
@@ -409,12 +470,12 @@ export const Header = () => {
                                                 </Link>
                                             </DropdownMenuTrigger>
                                             <DropdownMenuContent>
-                                                <div className="grid grid-cols-2 gap-3">
-                                                    <div className="grid-1">
-                                                        <DropdownMenuLabel className="px-5 text-base text-blue-900 uppercase font-bold">Loại</DropdownMenuLabel>
-                                                        <div className="px-3 duration-300 cursor-pointer">
+                                                <div className="grid grid-cols-2 gap-3 ">
+                                                    <div className="grid-1 border-r border-gray-100 px-5">
+                                                        <DropdownMenuLabel className="text-base text-blue-900 uppercase font-bold">Loại</DropdownMenuLabel>
+                                                        <div className=" duration-300 cursor-pointer">
                                                             <DropdownMenuItem>
-                                                                <Link className="flex gap-3 hover:text-yellow-400 duration-300 px-5 py-1 rounded-md">
+                                                                <Link to="/courses" className="flex gap-3 hover:text-yellow-400 duration-300 py-1 rounded-md">
                                                                     <svg width="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 8C4 5.17157 4 3.75736 4.87868 2.87868C5.75736 2 7.17157 2 10 2H14C16.8284 2 18.2426 2 19.1213 2.87868C20 3.75736 20 5.17157 20 8V16C20 18.8284 20 20.2426 19.1213 21.1213C18.2426 22 16.8284 22 14 22H10C7.17157 22 5.75736 22 4.87868 21.1213C4 20.2426 4 18.8284 4 16V8Z" stroke="currentColor" strokeWidth="2"></path><path d="M19.8978 16H7.89778C6.96781 16 6.50282 16 6.12132 16.1022C5.08604 16.3796 4.2774 17.1883 4 18.2235" stroke="currentColor" strokeWidth="2"></path><path d="M8 7H16" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path><path d="M8 10.5H13" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path></svg>
                                                                     <span className="font-semibold text-base">Tất cả khóa học</span>
                                                                 </Link>
@@ -422,9 +483,8 @@ export const Header = () => {
 
                                                         </div>
                                                     </div>
-                                                    <div className="grid-2">
-                                                        <DropdownMenuLabel className="px-5 text-base text-blue-900 uppercase font-bold">Danh mục</DropdownMenuLabel>
-
+                                                    <div className="grid-2 px-5">
+                                                        <DropdownMenuLabel className=" text-base text-blue-900 uppercase font-bold">Danh mục</DropdownMenuLabel>
                                                         {renderCategories()}
                                                     </div>
                                                 </div>
@@ -516,9 +576,33 @@ export const Header = () => {
                                                 {/* nav list */}
                                                 <ul className="max-xl:pt-3 gap-3 text-base xl:text-base text-left">
                                                     <li className="max-xl:mb-4">
-                                                        <Link to="/courses" className="hover:text-gray-500">
-                                                            Khóa học
-                                                        </Link>
+                                                        <DropdownMenu>
+                                                            <DropdownMenuTrigger>
+                                                                <Link to="/courses" className="hover:text-gray-500 border-none">
+                                                                    Khóa học
+                                                                </Link>
+                                                            </DropdownMenuTrigger>
+                                                            <DropdownMenuContent>
+                                                                <div className="px-5">
+                                                                    <div className="top">
+                                                                        <DropdownMenuLabel className=" text-base text-blue-900 uppercase font-bold">Loại</DropdownMenuLabel>
+                                                                        <div className=" duration-300 cursor-pointer">
+                                                                            <DropdownMenuItem>
+                                                                                <Link to="/courses" className="flex gap-3 hover:text-yellow-400 duration-300  py-1 rounded-md">
+                                                                                    <svg width="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 8C4 5.17157 4 3.75736 4.87868 2.87868C5.75736 2 7.17157 2 10 2H14C16.8284 2 18.2426 2 19.1213 2.87868C20 3.75736 20 5.17157 20 8V16C20 18.8284 20 20.2426 19.1213 21.1213C18.2426 22 16.8284 22 14 22H10C7.17157 22 5.75736 22 4.87868 21.1213C4 20.2426 4 18.8284 4 16V8Z" stroke="currentColor" strokeWidth="2"></path><path d="M19.8978 16H7.89778C6.96781 16 6.50282 16 6.12132 16.1022C5.08604 16.3796 4.2774 17.1883 4 18.2235" stroke="currentColor" strokeWidth="2"></path><path d="M8 7H16" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path><path d="M8 10.5H13" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path></svg>
+                                                                                    <span className="font-semibold text-base">Tất cả khóa học</span>
+                                                                                </Link>
+                                                                            </DropdownMenuItem>
+
+                                                                        </div>
+                                                                    </div>
+                                                                    <div className="bottom">
+                                                                        <DropdownMenuLabel className="text-base text-blue-900 uppercase font-bold">Danh mục</DropdownMenuLabel>
+                                                                        {renderCategories()}
+                                                                    </div>
+                                                                </div>
+                                                            </DropdownMenuContent>
+                                                        </DropdownMenu>
                                                     </li>
                                                     <li className="max-xl:mb-4">
                                                         <Link to="/contact" className="hover:text-gray-500">
