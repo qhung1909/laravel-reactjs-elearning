16326c685796140e9d6609948135e4932dbfaa83
Lâm Chấn Toàn <lamchantoan.sg@gmail.com>
2024-10-21 23:13:25 +0700

    usercontext instructor update

--- frontend/src/pages/usercontext/usercontext.jsx
+++ frontend/src/pages/usercontext/usercontext.jsx
@@ -21,13 +21,14 @@ const notify = (message, type) => {
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
+    const API_KEY = import.meta.env.VITE_API_KEY;
+    const API_URL = import.meta.env.VITE_API_URL;
     const [user, setUser] = useState(null);
     const [logined, setLogined] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState("");
     const [_success, setSuccess] = useState("");
-    const API_KEY = import.meta.env.VITE_API_KEY;
-    const API_URL = import.meta.env.VITE_API_URL;
+
     const navigate = useNavigate();
 
 
@@ -99,15 +100,44 @@ export const UserProvider = ({ children }) => {
         setUser(null);
         setLogined(null);
         notify('Đăng xuất thành công', 'success');
+        navigate('login')
     };
 
+    // update thông tin người dùng
+    const updateUserProfile = async (userName, email, avatar) => {
+        const token = localStorage.getItem("access_token");
+        if (!userName.trim()) {
+            notify('Tên tài khoản không được để trống', 'error');
+            return;
+        }
+        try {
+            const formData = new FormData();
+            formData.append('name', userName);
+            formData.append('email', email);
+            if (avatar) {
+                formData.append('file', avatar);
+            }
+            const response = await axios.post(`${API_URL}/auth/user/profile`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    Authorization: `Bearer ${token}`,
+                    'x-api-secret': `${API_KEY}`,
+                },
+            });
+            notify('Cập nhật thành công', 'success');
+            fetchUser(); // Refresh user data
+        } catch (error) {
+            console.log('Error updating profile', error);
+            notify("Cập nhật thất bại", 'error');
+        }
+    };
 
     useEffect(() => {
         fetchUser();
     }, []);
 
     return (
-        <UserContext.Provider value={{ user, logined, loading, fetchUser, logout, refreshToken }}>
+        <UserContext.Provider value={{ user, logined, loading, fetchUser, logout, refreshToken, updateUserProfile }}>
             {children}
             <Toaster/>
         </UserContext.Provider>
