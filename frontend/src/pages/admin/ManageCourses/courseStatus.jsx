e590d1338d6ae3dc76c1d17cdf44e014297c1cdb
Phuong Voong <voongngocphuong84@gmail.com>
2024-10-26 14:46:22 +0700

    update App.jsx

---
+++ frontend/src/pages/admin/ManageCourses/courseStatus.jsx
@@ -0,0 +1,48 @@
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "@/components/ui/sidebar"
+import { SideBarUI } from "../sidebarUI"
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+
+export const CourseStatus = () => {
+    return (
+        <>
+            <SidebarProvider>
+                <SideBarUI />
+                <SidebarInset>
+                    <header className="absolute left-1 top-3">
+                        {/* <header className="flex top-0 h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"> */}
+                        <div className="flex items-center gap-2 px-4">
+                            <SidebarTrigger className="-ml-1" />
+                            <Separator orientation="vertical" className="mr-2 h-4" />
+                            <Breadcrumb>
+                                <BreadcrumbList>
+                                    <BreadcrumbItem className="hidden md:block">
+                                        <BreadcrumbLink href="/">
+                                            Trang chủ
+                                        </BreadcrumbLink>
+                                    </BreadcrumbItem>
+                                    <BreadcrumbSeparator className="hidden md:block" />
+                                </BreadcrumbList>
+                            </Breadcrumb>
+                        </div>
+                    </header>
+
+                    {/* them noi dung vao day */}
+                    <div>Example</div>
+                    {/*=======================*/}
+
+                </SidebarInset>
+            </SidebarProvider>
+        </>
+    )
+}
