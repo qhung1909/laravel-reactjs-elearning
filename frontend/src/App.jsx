1ea720f20637b35db3b28deda210c21bb0389fde
BaoHungg <86723931+BaoHungg@users.noreply.github.com>
2024-11-09 21:23:04 +0700

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -64,6 +64,7 @@ import PersonalInformation from "./pages/admin/ManageAcount/PersonalInformation.
 import { ManageHeader } from "./pages/admin/ManageHeaderFooter/ManageHeader.jsx";
 import Draft from "./pages/admin/ManageCourses/draft.jsx";
 import ManageFooter from "./pages/admin/ManageHeaderFooter/ManageFooter.jsx";
+import Ourteam from "./pages/ourteam/ourteam.jsx";
 
 
 
@@ -147,6 +148,7 @@ function AppContent() {
                 <Route path="/detail" element={<Detail />}></Route>
                 <Route path="*" element={<PageNotFound />}></Route>
                 <Route path="/404" element={<PageNotFound />}></Route>
+                <Route path="/ourteam" element={<Ourteam />}></Route>
                 <Route path="/detail/:slug" element={<Detail />}></Route>
                 <Route path="/payment-result" element={<PaymentResult />}></Route>
                 <Route path="/payment" element={<Payment />}></Route>
