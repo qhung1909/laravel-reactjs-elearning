774a0048bcf7a9240322619944e7d59f7a6b4be2
Quoc Hung <quochung190904@gmail.com>
2024-11-12 14:56:48 +0700

    update

--- app/Http/Controllers/QuizOptionController.php
+++ app/Http/Controllers/QuizOptionController.php
@@ -409,9 +409,13 @@ class QuizOptionController extends Controller
     }
     public function index($questionId)
     {
-        $options = QuizOption::where('question_id', $questionId)->get();
+        $options = QuizOption::with('question')  
+                             ->where('question_id', $questionId)
+                             ->get();
+    
         return response()->json($options);
     }
+    
     
 
     public function store(Request $request, $questionId)
