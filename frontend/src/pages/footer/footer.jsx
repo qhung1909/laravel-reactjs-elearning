79046c82b9ee903e21d09bae4904f7cdf44f0b31
Lâm Chấn Toàn <lamchantoan.sg@gmail.com>
2024-12-03 12:22:50 +0700

    update

--- frontend/src/pages/footer/footer.jsx
+++ frontend/src/pages/footer/footer.jsx
@@ -9,17 +9,15 @@ export const Footer = () => {
 
                         {/* grid - left */}
                         <div className="col-span-1">
-                            {/* <div className="footer-start ">
-
-                            </div> */}
+            
                             <img
                                 src="/src/assets/images/antlearn.png"
                                 alt=""
                                 className="w-20 md:w-32 mb-5"
                             />
-                            <a href="https://www.facebook.com/profile.php?id=100079303916866">
-                                <img src="https://lmsantlearn.s3.ap-southeast-2.amazonaws.com/icons/New+folder/faecbooklogo.svg" className="w-7" alt="" />
-                            </a>
+                            <Link to="https://www.facebook.com/profile.php?id=100079303916866">
+                                <img src="https://lmsantlearn.s3.ap-southeast-2.amazonaws.com/icons/New+folder/faecbooklogo.svg" className="w-7"  alt="" />
+                            </Link>
                         </div>
 
                         {/* grid - right */}
@@ -27,29 +25,29 @@ export const Footer = () => {
                             <div className="flex items-start gap-5 xl:w-2/3 md:w-2/5">
                                 {/* thẻ 1 */}
                                 <div className="footer-mid-left md:w-1/3 sm:w-1/4 w-1/3">
-                                    <span className="font-medium text-black text-sm">
-                                        Sản phẩm
+                                    <span className="font-medium text-black text-base">
+                                        Link
                                     </span>
                                     <ul className="mt-1 space-y-1">
-                                        <li className="text-xs text-gray-500 md:leading-9 sm:leading-7 leading-loose"><Link to='/courses'>Khóa học</Link></li>
-                                        <li className="text-xs text-gray-500 md:leading-9 sm:leading-7 leading-loose"><Link to='/blog'>Bài viết</Link></li>
-                                        <li className="text-xs text-gray-500 md:leading-9 sm:leading-7 leading-loose"><Link to='/contact'>Liên hệ</Link></li>
+                                        <li className="text-sm text-gray-500 md:leading-9 sm:leading-7 leading-loose"><Link to='/courses'>Khóa học</Link></li>
+                                        <li className="text-sm text-gray-500 md:leading-9 sm:leading-7 leading-loose"><Link to='/blog'>Bài viết</Link></li>
+                                        <li className="text-sm text-gray-500 md:leading-9 sm:leading-7 leading-loose"><Link to='/contact'>Liên hệ</Link></li>
                                     </ul>
                                 </div>
 
                                 {/* thẻ 2 */}
                                 <div className="footer-mid-right">
-                                    <span className="font-medium text-black text-sm">
+                                    <span className="font-medium text-black text-base">
                                         Chính sách chung & Hỗ trợ
                                     </span>
                                     <ul className="mt-1 space-y-1">
                                         {/* term */}
                                         <Link to="/terms">
-                                            <li className="text-xs text-gray-500 md:leading-9 sm:leading-7 leading-loose">Điều khoản dịch vụ</li>
+                                            <li className="text-sm text-gray-500 md:leading-9 sm:leading-7 leading-loose">Điều khoản dịch vụ</li>
                                         </Link>
                                         {/* about us */}
                                         <Link to="/aboutus">
-                                            <li className="text-xs text-gray-500 md:leading-9 sm:leading-7 leading-loose">Về chúng tôi</li>
+                                            <li className="text-sm text-gray-500 md:leading-9 sm:leading-7 leading-loose">Về chúng tôi</li>
                                         </Link>
                                     </ul>
                                 </div>
@@ -58,7 +56,7 @@ export const Footer = () => {
                             {/* thẻ 3 */}
                             <div className="footer-end mt-5 md:mt-0">
                                 <div className="">
-                                    <span className="font-medium text-black text-sm">
+                                    <span className="font-medium text-black text-base">
                                         Ngôn ngữ
                                     </span>
                                     <div className="border border-gray-400 rounded-sm mt-3 md:w-full w-36">
@@ -66,7 +64,7 @@ export const Footer = () => {
                                             <div className="">
                                                 <img src="https://lmsantlearn.s3.ap-southeast-2.amazonaws.com/icons/New+folder/language.svg" className="w-6" alt="" />
                                             </div>
-                                            <div className="">
+                                            <div className="text-sm">
                                                 Tiếng Việt
                                             </div>
                                         </div>
@@ -79,7 +77,7 @@ export const Footer = () => {
 
                     </div>
                 </section>
-                <div className="bg-gray-100 text-center py-4">
+                <div className="bg-gray-100 text-center py-4 border-t-2">
                     <p className="text-gray-600 text-sm">
                         © {new Date().getFullYear()} Nhóm 4 - Dự án tốt nghiệp website Antlearn.
                     </p>
